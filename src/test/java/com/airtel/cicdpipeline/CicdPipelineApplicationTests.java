a20ca83374cafd697abbd8123cd32488d4cc5885
Rajat Chaudhary <Rajat.Chaudhary@airtel.com>
2021-12-27 22:57:31 +0530

    first commit

---
+++ src/test/java/com/airtel/cicdpipeline/CicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtel.cicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
